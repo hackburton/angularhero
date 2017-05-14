570ce97d04bda879056a766b11cacc506c36dc56
hackburton <hackburton@gmail.com>
2017-05-14 14:36:22 -0700

    Project Commit
    N/A

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+	{ id: 12, name: 'Narco' },
+	{ id: 13, name: 'Bobasto' },
+	{ id: 14, name: 'Celeritas' },
+	{ id: 15, name: 'Magneta' },
+	{ id: 16, name: 'Rubberman' },
+	{ id: 17, name: 'The Blob' },
+	{ id: 18, name: 'Torch' },
+	{ id: 19, name: 'Red Eyes' },
+	{ id: 20, name: 'Tornado' }
+];
